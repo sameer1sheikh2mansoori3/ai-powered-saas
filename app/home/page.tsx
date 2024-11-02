ad28cd798ec0724dfcc013837826d7786a6f0f6c
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-11-02 12:00:40 +0530

    added clerk middleware

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex h-screen justify-center items-center'>
+      <div className='bg-red-500 text-white'>help eme</div>
+    </div>
+  )
+}
+
+export default page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import daisyui from "daisyui"
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +14,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+
+  plugins: [daisyui,],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
 export default config;
